7c840aec934e7908bf14fc23e9d430b1b4f6e298
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2024-11-25 19:49:48 -0500

    Awwwards

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Hero from './components/Hero.jsx'
+import About from './components/About.jsx'
+import NavBar from './components/NavBar.jsx'
+import Features from './components/Features.jsx'
+import Story from './components/Story.jsx'
+import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
